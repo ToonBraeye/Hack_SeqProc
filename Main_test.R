33693433cccfb9d80f9453e6d747e20cc373be44
LouiseVaes <118883304+LouiseVaes@users.noreply.github.com>
2022-11-24 13:34:07 +0100

    Update Main_test.R
    Update 13.33

--- Main_test.R
+++ Main_test.R
@@ -1,10 +1,34 @@
 
 library(tidyverse)
+library(dplyr)
 
 Data <- readRDS("C:/Users/LoVa3397/Downloads/test_df (3).Rdata")
+ 
+Startperiod2   <- as.Date("2022-01-01")
+Startperiod <- as.Date("2022-01-01") - 90 
+Endperiod   <- as.Date("2022-07-31")
 
-Startperiod <- "2022-01-01"
-Endperiod   <- "2022-07-31"
-
-Data_long <- gather(Data, vaccin, day,date.vacc1:date.vacc2, factor_key=TRUE)
+Data_long <- gather(Data, vaccin, day, date.vacc1:date.vacc2, factor_key=TRUE)
 Data_long <- Data_long[complete.cases(Data_long),]
+
+Data_long_select <- Data_long[Data_long$day >= Startperiod & Data_long$day <= Endperiod, ]
+
+
+Data_long_select$date.90 <- if_else(Data_long_select$day + 90 <= Endperiod, 
+                                      Data_long_select$day + 90, as.Date(NA))
+Data_long_select$date.60 <- if_else(Data_long_select$day + 60 <= Endperiod, 
+                                      Data_long_select$day + 60, as.Date(NA))
+Data_long_select$date.30 <- if_else(Data_long_select$day + 30 <= Endperiod, 
+                                       Data_long_select$day + 30, as.Date(NA))
+Data_long_select_long <- gather(Data_long_select, status, day2,date.30:date.90, factor_key=FALSE)
+
+Data_long_select_long$status2 <- paste0(Data_long_select_long$vaccin,substr(Data_long_select_long$status,5,7))
+
+Data_part1 <- Data_long_select_long[,c(1,2,3)]
+Data_part2 <- Data_long_select_long[,c(1,5,6)] 
+colnames(Data_part2) <- c("id","day","vaccin")
+
+Data_complete <- bind_rows(Data_part1,Data_part2)
+
+Data_complete_select <- Data_complete[Data_complete$day >= Startperiod2 & Data_complete$day <= Endperiod, ]
+Data_complete_select <- Data_complete_select[complete.cases(Data_complete_select),]
