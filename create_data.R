0534c923e9087273f0928751ac9ed0f6817ff5df
ToonBraeye <toonbraeye@gmail.com>
2022-11-24 11:09:10 +0100

    initial commit

--- create_data.R
+++ create_data.R
@@ -9,3 +9,4 @@ test.df[, date.vacc2:=as.Date(ifelse(sample(1:2, 1000, replace=T) %% 2,
                                      as.Date(date.vacc1) + sample(c(1:365), replace=T, 1000), NA), origin='1970-01-01')]
 
 saveRDS(test.df, 'data/test_df.Rdata')
+write.csv(test.df, 'data/test_df.csv')
