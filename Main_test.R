0ff0aa562839e91dec56d0f77234cacc9c1a06e6
LouiseVaes <118883304+LouiseVaes@users.noreply.github.com>
2022-11-24 16:22:30 +0100

    Update Main_test.R
    16.22

--- Main_test.R
+++ Main_test.R
@@ -56,26 +56,37 @@ Data_full$Startperiod <- Startperiod2
 Data_full$Endperiod <- Endperiod
 
 Data_full_select <- data.frame(id=Data_full$id)
-Data_full_select$date.vacc1 <- if_else(Data_full$date.vacc1 >= Startperiod2 & Data_full$date.vacc1 <= Endperiod, Data_full$date.vacc1, as.Date(NA))
-Data_full_select$date.vacc1_30 <- if_else(Data_full$date.vacc1_30 >= Startperiod2 & Data_full$date.vacc1_30 <= Endperiod, Data_full$date.vacc1_30, as.Date(NA))
-Data_full_select$date.vacc1_60 <- if_else(Data_full$date.vacc1_60 >= Startperiod2 & Data_full$date.vacc1_60 <= Endperiod, Data_full$date.vacc1_60, as.Date(NA))
-Data_full_select$date.vacc1_90 <- if_else(Data_full$date.vacc1_90 >= Startperiod2 & Data_full$date.vacc1_90 <= Endperiod, Data_full$date.vacc1_90, as.Date(NA))
+Data_full_select$B <- if_else(Data_full$date.vacc1 >= Startperiod2 & Data_full$date.vacc1 <= Endperiod, Data_full$date.vacc1, as.Date(NA))
+Data_full_select$C <- if_else(Data_full$date.vacc1_30 >= Startperiod2 & Data_full$date.vacc1_30 <= Endperiod, Data_full$date.vacc1_30, as.Date(NA))
+Data_full_select$D <- if_else(Data_full$date.vacc1_60 >= Startperiod2 & Data_full$date.vacc1_60 <= Endperiod, Data_full$date.vacc1_60, as.Date(NA))
+Data_full_select$E <- if_else(Data_full$date.vacc1_90 >= Startperiod2 & Data_full$date.vacc1_90 <= Endperiod, Data_full$date.vacc1_90, as.Date(NA))
 
-Data_full_select$date.vacc2 <- if_else(Data_full$date.vacc2 >= Startperiod2 & Data_full$date.vacc2 <= Endperiod, Data_full$date.vacc2, as.Date(NA))
-Data_full_select$date.vacc2_30 <- if_else(Data_full$date.vacc2_30 >= Startperiod2 & Data_full$date.vacc2_30 <= Endperiod, Data_full$date.vacc2_30, as.Date(NA))
-Data_full_select$date.vacc2_60 <- if_else(Data_full$date.vacc2_60 >= Startperiod2 & Data_full$date.vacc2_60 <= Endperiod, Data_full$date.vacc2_60, as.Date(NA))
-Data_full_select$date.vacc2_90 <- if_else(Data_full$date.vacc2_90 >= Startperiod2 & Data_full$date.vacc2_90 <= Endperiod, Data_full$date.vacc2_90, as.Date(NA))
-Data_full_select$startperiod <- Startperiod2
-Data_full_select$endperiod <- Endperiod
+Data_full_select$F <- if_else(Data_full$date.vacc2 >= Startperiod2 & Data_full$date.vacc2 <= Endperiod, Data_full$date.vacc2, as.Date(NA))
+Data_full_select$G <- if_else(Data_full$date.vacc2_30 >= Startperiod2 & Data_full$date.vacc2_30 <= Endperiod, Data_full$date.vacc2_30, as.Date(NA))
+Data_full_select$H <- if_else(Data_full$date.vacc2_60 >= Startperiod2 & Data_full$date.vacc2_60 <= Endperiod, Data_full$date.vacc2_60, as.Date(NA))
+Data_full_select$I <- if_else(Data_full$date.vacc2_90 >= Startperiod2 & Data_full$date.vacc2_90 <= Endperiod, Data_full$date.vacc2_90, as.Date(NA))
+Data_full_select$A <- Startperiod2
+Data_full_select$J <- Endperiod
 
-Data_long <- gather(Data_full_select, status, day, date.vacc1:endperiod, factor_key=FALSE)
+# A = startperiod
+# B = date.vacc1
+# C = date.vacc1.30
+# D = date.vacc1.60
+# E = date.vacc1.90
+# F = date.vacc2
+# G = date.vacc2.30
+# H = date.vacc2.60
+# I = date.vacc2.90
+# J = endperiod
+
+Data_long <- gather(Data_full_select, status, day, B:J, factor_key=FALSE)
 Data_long <- Data_long[complete.cases(Data_long),]
 
-Data_long <- Data_long[order(Data_long$id, Data_long$day),]
+Data_long <- Data_long[order(Data_long$id, Data_long$status),]
 Data_long$status2 <- shift(Data_long$status,1,"id")
 Data_long$day2 <- shift(Data_long$day,1,"id")
 
-Data_long <- Data_long[!(Data_long$status == "endperiod" & Data_long$status2 == "startperiod"),]
+Data_long <- Data_long[!(Data_long$status == "J" & Data_long$status2 == "A"),]
 
 Data_long$state <- paste0(Data_long$status, "_",Data_long$status2)
 
